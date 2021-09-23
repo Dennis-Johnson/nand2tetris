b23091f7dc43a6c8d0cd821ae39f7265d282ef3d
Dennis Johnson <densarjohn@gmail.com>
2021-09-23 11:27:25 +0530

    Multiplication in Hack assembly

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,47 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+    @i
+    M = 0     // i = 0
+
+    @prod
+    M = 0     // prod = 0
+
+(LOOP)
+    @i
+    D = M     // D = i
+
+    @R0
+    D = D- M    //  D = i - R0
+
+    @STORE 
+    D; JGE      // JMP to STORE if (i - R0) >= 0
+
+    @R1
+    D = M       // D = R1
+
+    @prod
+    M = D + M   // prod = R1 + prod
+
+
+    @i
+    M = M + 1   // i = i + 1
+
+    @LOOP
+    0; JMP
+
+(STORE)
+    @prod
+    D = M       // D = prod
+    
+    @R2
+    M = D       // R2 = prod
+
+(END)
+    @END
+    0; JMP
+
+
+
+
+
