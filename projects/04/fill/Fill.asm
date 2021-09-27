2fd51962b7e87dbe6f1ebf0c3624a99fd79a1af4
Dennis Johnson <densarjohn@gmail.com>
2021-09-27 21:52:17 +0530

    Upperbound for SCREEN address was off by one, was accidently overwriting KBD memorymap at the end of SCREEN

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,8 +11,20 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-    
+(OUTERLOOP)
+    @fillValue
+    M = 0               // Default fillValue is 0. Used to fill white screen
 
+    @KBD
+    D = M               // Poll RAM[24576] for keyboard scan code.
+
+    @FILL
+    D; JEQ              // Jump to FILL if Keyboard scancode == 0. No key was pressed.
+
+    @fillValue
+    M = -1              // Set fillValue to -1, which is black in 2s complement binary (1111111111111111)
+
+(FILL)
     @SCREEN
     D = A               // D = RAM[16384], the base address of the screen.
 
@@ -22,25 +34,28 @@
     @i
     M = 0               // i = 0. Counter for fill loop.
 
-    @8192
-    D = A               // D = 8K, the whole screen.
+    @8191
+    D = A               // D = 8K
 
     @n
-    M = D               // n = 64, number of rows to fill.
+    M = D               // n = 8K, the whole screen.
 
-(LOOP)
+(DRAWLOOP)
     @i
     D = M                // D = i
 
     @n
     D = D - M            // D = i - n
 
-    @END
-    D; JGT               // Jump to END if (i - n) > 0
+    @OUTERLOOP
+    D; JGT               // Jump to OUTERLOOP if (i - n) > 0. Done filling the screen.
+
+    @fillValue  
+    D = M                // D = fillValue
 
     @screenAddress
     A = M                // A = screenAddress
-    M = -1               // fill with black at RAM[A]
+    M = D                // fill with the fillValue
 
     @screenAddress
     D = M                // D = screenAddress      
@@ -49,7 +64,7 @@
     @i
     M = M + 1            // i += 1
 
-    @LOOP
+    @DRAWLOOP
     0; JMP
 
 (END)
